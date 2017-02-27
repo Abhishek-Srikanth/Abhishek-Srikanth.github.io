d67cdef7c63294bbee8344f4d140422212c11f02
Abhishek Srikanth <srikanth@purdue.edu>
2017-02-27 01:05:36 -0500

    updated link from beta to prodgit status!

--- apurvaKeyboard/common.js
+++ apurvaKeyboard/common.js
@@ -1,4 +1,5 @@
 function install()
 {
-    window.open("https://play.google.com/apps/testing/com.bsac.apurvaKeyboard");
+    window.open("https://play.google.com/store/apps/details?id=com.bsac.apurvaKeyboard");
+    //window.open("https://play.google.com/apps/testing/com.bsac.apurvaKeyboard");
 }
